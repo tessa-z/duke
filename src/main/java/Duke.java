cb64fbea3016734657c6e5398b7939bbc17c240e
tessa-z <yuehan9902@gmail.com>
2019-08-22 23:35:01 +0800

    A-Classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
 
 //        Pair<String, Integer>;
-        ArrayList<String[]> tasks = new ArrayList<String[]>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         while (true) {
             Scanner in = new Scanner(System.in);
@@ -27,21 +27,21 @@ public class Duke {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Here are the tasks in your list:");
                 int count = 0;
-                for (String[] task : tasks) {
+                for (Task task : tasks) {
                     count++;
-                    System.out.println("     " + count + ".[" + task[1] + "] " + task[0]);
+                    System.out.println("     " + count + ".[" + task.getStatusIcon() + "] " + task.description);
                 }
                 System.out.println("    ____________________________________________________________");
             } else if (input.contains("done")) {
                 String complete[] = input.split(" ");
-                tasks.get(Integer.parseInt(complete[1])-1)[1] = "✓";
+                tasks.get(Integer.parseInt(complete[1]) - 1).markAsDone();
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       [" + tasks.get(Integer.parseInt(complete[1])-1)[1] +  "] " + tasks.get(Integer.parseInt(complete[1])-1)[0]);
+                System.out.println("       [" + tasks.get(Integer.parseInt(complete[1]) - 1).getStatusIcon() + "] " + tasks.get(Integer.parseInt(complete[1]) - 1).description);
                 System.out.println("    ____________________________________________________________");
             } else {
-                String[] newtask = {input, "✗"};
-                tasks.add(newtask);
+                Task t = new Task(input);
+                tasks.add(t);
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     added: " + input);
                 System.out.println("    ____________________________________________________________");
@@ -50,3 +50,5 @@ public class Duke {
 
     }
 }
+
+
