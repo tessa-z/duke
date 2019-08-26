5362a0060f56e0b16f59c0e1ce9ecea6e104ba21
tessa-z <yuehan9902@gmail.com>
2019-08-26 19:42:28 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,66 +14,113 @@ public class Duke {
         while (true) {
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
+            String taskInfo[] = input.split(" ", 2);
             if (input.equals("bye")) {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
                 break;
-            } else if (input.contains("done")) {
-                String complete[] = input.split(" ");
-                tasks.get(Integer.parseInt(complete[1]) - 1).markAsDone();
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       [" + tasks.get(Integer.parseInt(complete[1]) - 1).getStatusIcon() + "] " + tasks.get(Integer.parseInt(complete[1]) - 1).description);
-                System.out.println("    ____________________________________________________________");
-            } else if (input.contains("todo")) {
-                String taskInfo[] = input.split(" ", 2);
-                Todo t = new Todo(taskInfo[1]);
-                tasks.add(t);
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Got it. I've added this task:");
-                System.out.println("       [T][✘] " + taskInfo[1]);
-                System.out.println("     Now you have " + tasks.size() + " task(s) in the list.");
-                System.out.println("    ____________________________________________________________");
-            } else if (input.contains("deadline")) {
-                String parse[] = input.split(" ", 2);
-                String taskInfo[] = parse[1].split("/");
-                String dateInfo[] = taskInfo[1].split(" ", 2);
-                Deadline t = new Deadline(taskInfo[0], dateInfo[1]);
-                tasks.add(t);
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Got it. I've added this task:");
-                System.out.println("       [D][✘] " + taskInfo[0] + "(by: " + dateInfo[1] + ")");
-                System.out.println("     Now you have " + tasks.size() + " task(s) in the list.");
-                System.out.println("    ____________________________________________________________");
-            } else if (input.contains("event")) {
-                String parse[] = input.split(" ", 2);
-                String taskInfo[] = parse[1].split("/");
-                String dateInfo[] = taskInfo[1].split(" ", 2);
-                Event t = new Event(taskInfo[0], dateInfo[1]);
-                tasks.add(t);
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Got it. I've added this task:");
-                System.out.println("       [E][✘] " + taskInfo[0] + "(at: " + dateInfo[1] + ")");
-                System.out.println("     Now you have " + tasks.size() + " task(s) in the list.");
-                System.out.println("    ____________________________________________________________");
-            } else if (input.equals("list") && (tasks.isEmpty())) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Task list is empty.");
-                System.out.println("    ____________________________________________________________");
-            } else if (input.equals("list")) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Here are the tasks in your list:");
-                int count = 0;
-                for (Task task : tasks) {
-                    ++count;
-                    System.out.println("     " + count + "." + task.toString());
+            } else if (taskInfo[0].equals("done")) {
+                try {
+                    if ((taskInfo.length < 2) || (taskInfo[1].equals(""))) {
+                        DukeException e = new DukeException();
+                        throw e;
+                    }
+                    tasks.get(Integer.parseInt(taskInfo[1]) - 1).markAsDone();
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     Nice! I've marked this task as done:");
+                    System.out.println("       [" + tasks.get(Integer.parseInt(taskInfo[1]) - 1).getStatusIcon() + "] " + tasks.get(Integer.parseInt(taskInfo[1]) - 1).description);
+                    System.out.println("    ____________________________________________________________");
+                } catch (DukeException e) {
+                    System.out.println("    ____________________________________________________________");
+                    e.incompleteFields(DukeException.ErrorType.DONE);
+                    System.out.println("    ____________________________________________________________");
                 }
-                System.out.println("    ____________________________________________________________");
+            } else if (taskInfo[0].equals("todo")) {
+                try {
+                    if ((taskInfo.length < 2) || (taskInfo[1].equals(""))) {
+                        DukeException e = new DukeException();
+                        throw e;
+                    }
+                    Todo t = new Todo(taskInfo[1]);
+                    tasks.add(t);
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     Got it. I've added this task:");
+                    System.out.println("       [T][✘] " + taskInfo[1]);
+                    System.out.println("     Now you have " + tasks.size() + " task(s) in the list.");
+                    System.out.println("    ____________________________________________________________");
+                } catch (DukeException e) {
+                    System.out.println("    ____________________________________________________________");
+                    e.incompleteFields(DukeException.ErrorType.TODO);
+                    System.out.println("    ____________________________________________________________");
+                }
+            } else if (taskInfo[0].equals("deadline")) {
+                String dateInfo[] = taskInfo[1].split("/by");
+                try {
+                    if ((dateInfo.length < 2) || (dateInfo[1].equals(" "))) {
+                        DukeException e = new DukeException();
+                        throw e;
+                    }
+                    Deadline t = new Deadline(dateInfo[0], dateInfo[1]);
+                    tasks.add(t);
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     Got it. I've added this task:");
+                    System.out.println("       [D][✘] " + dateInfo[0] + "(by: " + dateInfo[1] + ")");
+                    System.out.println("     Now you have " + tasks.size() + " task(s) in the list.");
+                    System.out.println("    ____________________________________________________________");
+                } catch (DukeException e) {
+                    System.out.println("    ____________________________________________________________");
+                    e.incompleteFields(DukeException.ErrorType.DEADLINE);
+                    System.out.println("    ____________________________________________________________");
+                }
+            } else if (taskInfo[0].equals("event")) {
+                String dateInfo[] = taskInfo[1].split("/at");
+                try {
+                    if ((dateInfo.length < 2) || (dateInfo[1].equals(" "))) {
+                        DukeException e = new DukeException();
+                        throw e;
+                    }
+                    Event t = new Event(dateInfo[0], dateInfo[1]);
+                    tasks.add(t);
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     Got it. I've added this task:");
+                    System.out.println("       [E][✘] " + dateInfo[0] + "(at: " + dateInfo[1] + ")");
+                    System.out.println("     Now you have " + tasks.size() + " task(s) in the list.");
+                    System.out.println("    ____________________________________________________________");
+                } catch (DukeException e) {
+                    System.out.println("    ____________________________________________________________");
+                    e.incompleteFields(DukeException.ErrorType.EVENT);
+                    System.out.println("    ____________________________________________________________");
+                }
+            } else if (taskInfo[0].equals("list")) {
+                try {
+                    if (tasks.isEmpty()) {
+                        DukeException e = new DukeException();
+                        throw e;
+                    }
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     Here are the tasks in your list:");
+                    int count = 0;
+                    for (Task task : tasks) {
+                        ++count;
+                        System.out.println("     " + count + "." + task.toString());
+                    }
+                    System.out.println("    ____________________________________________________________");
+
+                } catch (DukeException e){
+                    System.out.println("    ____________________________________________________________");
+                    e.listEmpty();
+                    System.out.println("    ____________________________________________________________");
+                }
             } else {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Invalid input. Please try again.");
-                System.out.println("    ____________________________________________________________");
+                try {
+                    DukeException e = new DukeException();
+                    throw e;
+                } catch (DukeException e){
+                    System.out.println("    ____________________________________________________________");
+                    e.invalidCommand();
+                    System.out.println("    ____________________________________________________________");
+                }
             }
         }
 
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,32 @@
+public class DukeException extends Exception {
+    DukeException() {}
+
+    //if list command but list is empty
+    public void listEmpty() {
+        System.out.println("     ☹ WOT? Task list is empty. Please input a task before\n       viewing list.");
+    }
+
+    //if command is invalid
+    public void invalidCommand() {
+        System.out.println("     ☹ WOT? I don't understand what you have entered.");
+    }
+
+    enum ErrorType {
+        DONE,
+        TODO,
+        DEADLINE,
+        EVENT
+    }
+
+    //if incomplete fields for task commands
+    public void incompleteFields(ErrorType error) {
+        switch(error) {
+            case DONE: System.out.println("     ☹ WOT? Expected format: \"done [index of task]\""); break;
+            case TODO: System.out.println("     ☹ WOT? Expected format: \"todo [description of task]\""); break;
+            case DEADLINE: System.out.println("     ☹ WOT? Expected format: \"deadline [description of task] /by [date &/ time]\""); break;
+            case EVENT: System.out.println("     ☹ WOT? Expected format: \"event [description of event] /at [date &/ time]"); break;
+        }
+    }
+
+}
+
