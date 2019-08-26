cbad5c000081713092350817c954ccab2df4ab5e
tessa-z <yuehan9902@gmail.com>
2019-08-26 16:51:27 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,6 +19,43 @@ public class Duke {
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
                 break;
+            } else if (input.contains("done")) {
+                String complete[] = input.split(" ");
+                tasks.get(Integer.parseInt(complete[1]) - 1).markAsDone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       [" + tasks.get(Integer.parseInt(complete[1]) - 1).getStatusIcon() + "] " + tasks.get(Integer.parseInt(complete[1]) - 1).description);
+                System.out.println("    ____________________________________________________________");
+            } else if (input.contains("todo")) {
+                Task t = new Task(input, "T");
+                tasks.add(t);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       [T][✘] " + input);
+                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
+            } else if (input.contains("deadline")) {
+                String parse[] = input.split(" ", 2);
+                String taskInfo[] = parse[1].split("/");
+                String dateInfo[] = taskInfo[1].split(" ", 2);
+                Task t = new Task(taskInfo[0], "D", taskInfo[1]);
+                tasks.add(t);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       [D][✘] " + taskInfo[0] + " (by: " + dateInfo[1] + ")");
+                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
+            } else if (input.contains("event")) {
+                String parse[] = input.split(" ", 2);
+                String taskInfo[] = parse[1].split("/");
+                String dateInfo[] = taskInfo[1].split(" ", 2);
+                Task t = new Task(taskInfo[0], "E", dateInfo[1]);
+                tasks.add(t);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       [E][✘] " + taskInfo[0] + " (at: " + dateInfo[1] + ")");
+                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
             } else if (input.equals("list") && (tasks.isEmpty())) {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Task list is empty.");
@@ -29,21 +66,12 @@ public class Duke {
                 int count = 0;
                 for (Task task : tasks) {
                     count++;
-                    System.out.println("     " + count + ".[" + task.getStatusIcon() + "] " + task.description);
+                    System.out.println("     " + count + ".[" + task.type + "]" + "[" + task.getStatusIcon() + "] " + task.description);
                 }
                 System.out.println("    ____________________________________________________________");
-            } else if (input.contains("done")) {
-                String complete[] = input.split(" ");
-                tasks.get(Integer.parseInt(complete[1]) - 1).markAsDone();
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       [" + tasks.get(Integer.parseInt(complete[1]) - 1).getStatusIcon() + "] " + tasks.get(Integer.parseInt(complete[1]) - 1).description);
-                System.out.println("    ____________________________________________________________");
             } else {
-                Task t = new Task(input);
-                tasks.add(t);
                 System.out.println("    ____________________________________________________________");
-                System.out.println("     added: " + input);
+                System.out.println("     Invalid input. Please try again.");
                 System.out.println("    ____________________________________________________________");
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,19 @@
 public class Task {
     protected String description;
+    protected String type;
+    protected String date;
     protected boolean isDone;
 
-    public Task(String description) {
+    public Task(String description, String type) {
         this.description = description;
+        this.type = type;
+        this.isDone = false;
+    }
+
+    public Task(String description, String type, String date) {
+        this.description = description;
+        this.type = type;
+        this.date = date;
         this.isDone = false;
     }
 
