204ae0e2890293830fadfe7272563ab0b80d526a
Tessa Zhang <yuehan9902@gmail.com>
2019-09-03 16:58:05 +0800

    Fix bug in runtest.sh.

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp /home/tessa/Documents/CS2113/duke/src -Xlint:none -d /home/tessa/Documents/CS2113/duke/bin ~/Documents/CS2113/duke/src/main/java/Duke.java
+if ! javac -cp /home/tessa/Documents/CS2113/duke/src -Xlint:none -d /home/tessa/Documents/CS2113/duke/bin ~/Documents/CS2113/duke/src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
